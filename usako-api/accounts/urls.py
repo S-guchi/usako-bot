be8f82eb3dd6ef5609c8c65a03661c6194dc7eee
shoya <cook.shoya@gmail.com>
2022-07-27 10:14:57 +0900

    space crud

--- usako-api/accounts/urls.py
+++ usako-api/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CrudAccount
+from .views import CrudAccount, CrudSpace
 
 urlpatterns = [
-    path("v1/", CrudAccount.as_view()),
+    path("v1/account", CrudAccount.as_view()),
+    path("v1/space", CrudSpace.as_view()),
 ]
--- usako-api/accounts/views.py
+++ usako-api/accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.hashers import make_password
 from rest_framework import status, exceptions
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from django.db import transaction
 import traceback
 from .models import Space, User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, SpaceSerializer
 
 
 class CrudAccount(RetrieveUpdateDestroyAPIView):
@@ -98,6 +99,115 @@ class CrudAccount(RetrieveUpdateDestroyAPIView):
             user = get_object_or_404(User, line_id=request.query_params["line_id"])
 
             user.delete()
+
+            return Response(
+                status=status.HTTP_200_OK,
+            )
+
+        except Exception:
+            print(traceback.format_exc())
+            return Response(
+                data={"error-message": "不明なエラーが発生しました。"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class CrudSpace(RetrieveUpdateDestroyAPIView):
+    serializer_class = SpaceSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+
+            if "id" in request.GET:
+                # query_paramが指定されている場合の処理
+                id = request.GET.get("id")
+            else:
+                return Response(
+                    data={"error-message": "idが必要です。"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            space = Space.objects.get(pk=id)
+            serializer = SpaceSerializer(space)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+            )
+        except Space.DoesNotExist:
+            return Response(
+                data={"error-message": "spaceが見つかりませんでした。"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        except Exception:
+            print(traceback.format_exc())
+            return Response(
+                data={"error-message": "不明なエラーが発生しました。"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    def put(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            validated_data = serializer.validated_data
+
+            space = Space.objects.create()
+            space.name = validated_data.get("name")
+            space.password = make_password(validated_data.get("password"))
+
+            space.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+        except exceptions.ValidationError as e:
+            print(traceback.format_exc())
+            return Response(
+                data={"error-message": e.detail},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+            # return Response()
+        except Exception:
+            print(traceback.format_exc())
+            return Response(
+                data={"error-message": "不明なエラーが発生しました。"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    def patch(self, request, *args, **kwargs):
+        # https://qiita.com/aja_min/items/a84b00c74225e41e7da5
+        try:
+
+            space = get_object_or_404(Space, id=request.data["id"])
+
+            serializer = self.get_serializer(
+                instance=space, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+
+        except Exception:
+            print(traceback.format_exc())
+            return Response(
+                data={"error-message": "不明なエラーが発生しました。"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    def delete(self, request, *args, **kwargs):
+        try:
+
+            space = get_object_or_404(Space, id=request.query_params["id"])
+
+            space.delete()
 
             return Response(
                 status=status.HTTP_200_OK,
